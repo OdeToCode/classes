9d2a93f080a3661bc8dc3bae7c2d365bd303ac91
K. Scott Allen <scott@OdeToCode.com>
2011-11-01 17:59:10 -0400

    deck 02 forms

--- slides/point5.js
+++ slides/point5.js
@@ -104,7 +104,7 @@ $(function () {
 	
 	$("section > h1").addClass("row span16 center");
 	$("section > ul").addClass("row span16");
-	$("section > pre").addClass("row span16 prettyprint");
+	$("section pre").addClass("row span16 prettyprint");
 	
     prettyPrint();
 	p5.startShow();
