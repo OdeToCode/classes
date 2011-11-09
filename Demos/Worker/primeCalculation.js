559e1c33bd11bfb0adb23e90b7089381d585c41e
K. Scott Allen <scott@OdeToCode.com>
2011-11-08 19:26:37 -0500

    webworkers and websockets

---
+++ Demos/Worker/primeCalculation.js
@@ -0,0 +1,17 @@
+﻿var isPrime = true;
+var n = 1000000000;
+
+while (true) {
+    n += 1;
+    isPrime = true;
+    for (var i = 2; i <= Math.sqrt(n); i += 1) {
+        if (n % i == 0) {
+            isPrime = false;
+            continue;
+        }
+    }
+    // found a prime!
+    if (isPrime) {
+        postMessage(n);
+    }
+}
---
+++ Labs/08_workers/after/worker.js
@@ -0,0 +1,18 @@
+﻿self.onmessage = function (event) {
+    findPrimeNumber(event.data.min, event.data.max);    
+};
+
+function findPrimeNumber(min, max) {          
+    for(var n = min; n < max; n++) {          
+        var isPrime = true;
+        for(var i = 2; i < Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                isPrime = false;
+                continue;
+            }
+        }
+        if(isPrime) {
+            postMessage(n);
+        }
+    }
+}
