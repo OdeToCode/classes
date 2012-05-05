7ff13674172904cc6be59735842673d3fc238595
K. Scott Allen <scott@OdeToCode.com>
2012-05-05 08:43:28 -0400

    DbContext lab added

---
+++ linq/labs/LINQ_DbContext/after/EmployeeTimeCards/EmployeeTimeCards.Web/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeTimeCards.Core;
+using EmployeeTimeCards.Web.Infrastructure;
+
+namespace EmployeeTimeCards.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new TimeCardContext();
+            var model = db.Employees;
+
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var db = new TimeCardContext();
+            var model = db.Employees.Find(id);
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new TimeCardContext();
+            var model = db.Employees.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee updatedEmployee)
+        {
+            if(ModelState.IsValid)
+            {
+                var db = new TimeCardContext();
+                db.Entry(updatedEmployee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(updatedEmployee);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee newEmployee)
+        {
+            if(ModelState.IsValid)
+            {
+                var db = new TimeCardContext();
+                db.Employees.Add(newEmployee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(newEmployee);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var db = new TimeCardContext();
+            var model = db.Employees.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            var db = new TimeCardContext();
+            var employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ linq/labs/LINQ_DbContext/after/EmployeeTimeCards/EmployeeTimeCards.Web/Infrastructure/TimeCardContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using EmployeeTimeCards.Core;
+
+namespace EmployeeTimeCards.Web.Infrastructure
+{    
+    public class TimeCardContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<PayPeriod> PayPeriods { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                        .Property(e => e.FirstName)
+                        .HasMaxLength(80);
+                                    
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
