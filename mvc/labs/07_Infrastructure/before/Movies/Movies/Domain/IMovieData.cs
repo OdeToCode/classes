a7464bcfd8eb10d8277e1c5da2ff1ad8910422b7
K. Scott Allen <scott@OdeToCode.com>
2012-08-18 07:02:27 -0400

    lab 07 infrastructure done

---
+++ mvc/labs/07_Infrastructure/before/Movies/Movies/Domain/IMovieData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Domain
+{
+    public interface IMovieData
+    {
+        IQueryable<Review> Reviews { get; }
+        IQueryable<Movie> Movies { get; }
+
+        void SaveChanges();
+        void AddOrAttach<T>(T entity) where T : class;
+    }
+}
--- mvc/labs/ex02_DbContext/after/EmployeeTimeCards/EmployeeTimeCards.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmployeeTimeCards.Web.MvcApplication" Language="C#" %>
