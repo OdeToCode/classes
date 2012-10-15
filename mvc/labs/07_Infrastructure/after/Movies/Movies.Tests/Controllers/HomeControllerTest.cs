5672f4b57ddc2510398171970f2f920822053529
Scott Allen <scott@OdeToCode.com>
2012-10-15 09:41:26 -0400

    update with EF evolution

--- mvc/labs/07_Infrastructure/after/Movies/Movies.Tests/Controllers/HomeControllerTest.cs
+++ mvc/labs/07_Infrastructure/after/Movies/Movies.Tests/Controllers/HomeControllerTest.cs
@@ -12,18 +12,18 @@ namespace Movies.Tests.Controllers
     [TestClass]
     public class HomeControllerTest
     {
-        [TestMethod]
-        public void Index()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
+        //[TestMethod]
+        //public void Index()
+        //{
+        //    // Arrange
+        //    HomeController controller = new HomeController();
 
-            // Act
-            ViewResult result = controller.Index() as ViewResult;
+        //    // Act
+        //    ViewResult result = controller.Index() as ViewResult;
 
-            // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
-        }
+        //    // Assert
+        //    Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+        //}
 
         [TestMethod]
         public void About()
