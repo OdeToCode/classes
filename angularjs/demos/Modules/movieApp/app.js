620e1b333134b5d09a78316b71753b2785c5c9d7
K. Scott Allen <scott@odetocode.com>
2013-11-25 01:16:12 -0500

    slides continue

---
+++ angularjs/demos/Modules/movieApp/app.js
@@ -0,0 +1,16 @@
+﻿"use strict";    
+(function () {
+
+    var app = angular.module("movieApp", []);
+
+    app.config(function () {
+        // configure the application
+        //  example: configure routes
+    });
+
+    app.run(function ($rootScope) {
+        $rootScope.message = "Hi!";
+    });
+
+}());
+
---
+++ angularjs/demos/Modules/movieApp/controllers/movieListController.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+(function (app) {
+
+    var MovieListController = function ($scope) {
+
+        $scope.movies = [
+            { title: "Star Wars" },
+            { title: "Hunger Games" },
+            { title: "Indiana Jones" }
+        ];
+    };
+
+    app.controller("MovieListController", MovieListController);
+
+}(angular.module("movieApp")));
+
+
