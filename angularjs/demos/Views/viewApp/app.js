3344910cbe38661ff80dbee9836dc3d4cb3fc7a1
K. Scott Allen <scott@odetocode.com>
2013-11-29 00:46:44 -0500

    module 3

---
+++ angularjs/demos/Views/viewApp/app.js
@@ -0,0 +1,52 @@
+﻿(function(app) {
+
+    app.run(function($rootScope) {
+
+    	$rootScope.count = 1;
+
+        $rootScope.$on("myEmitEvent", function(event, args) {
+            $rootScope.count += args.count;
+        });
+
+        $rootScope.broadcastMessage = function() {
+        	$rootScope.$broadcast("myBroadcastEvent", {
+                a: "b"
+            });
+        };
+    });
+
+    var SimpleController = function($scope, simpleService) {
+
+        var incrementCount = function() {
+
+            $scope.$apply(function() {
+                $scope.count += 1;
+            });
+        	
+            setTimeout(incrementCount, 1000);
+        };
+        setTimeout(incrementCount, 1000);
+
+    	$scope.count = 1;
+        
+        $scope.$on("myBroadcastEvent", function() {
+            $scope.args = arguments;
+        });
+
+        $scope.emitMessage = function() {
+        	$scope.$emit("myEmitEvent", { count: 1 });
+            simpleService.getValue()
+                .then(function(result) {
+                    $scope.simpleResult = result;
+                });
+        };
+
+        $scope.changeMessage = function() {
+            $scope.message = "Hello, " + $scope.firstName;
+        };
+    };
+
+    app.controller("SimpleController", SimpleController);
+   
+
+}(angular.module("viewApp", [])));
