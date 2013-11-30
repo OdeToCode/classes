0504bd3fffa2f7c096155dd5e37aa28274667d68
K. Scott Allen <scott@odetocode.com>
2013-11-30 13:06:47 -0500

    tweaks

---
+++ angularjs/demos/Animate/movieApp/controllers/movieListController.js
@@ -0,0 +1,34 @@
+﻿"use strict";
+(function (app) {
+   
+    var MovieListController = function ($scope, movieService, $http) {
+                                        
+        var fetchMovies = function() {
+            movieService
+            .getAll()
+            .then(function (result) {
+                $scope.movies = result.data;
+            });
+        };
+
+        fetchMovies();
+
+        $scope.add = function() {
+            $scope.movies.push({ title: "New Movie" });
+        };
+
+        $scope.delete = function(index) {
+            $scope.movies.splice(index, 1);
+        };
+
+        $scope.refresh = function() {
+            fetchMovies();
+        };
+        $scope.http = $http.defaults;
+    };
+
+    app.controller("MovieListController", MovieListController);
+
+}(angular.module("movieApp")));
+
+
