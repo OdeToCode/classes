aaff9c97d4bbb04e2fac17df4d9acc4f44e630f4
Scott Allen <scott@odetocode.com>
2019-08-30 06:37:46 -0400

    update aspnetcore m3

---
+++ aspnetcore/labs/03/CoreMovies/Middlewares/ApplicationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using CoreMovies.Web.Middlewares;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseDefaultCsp(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<DefaultCsp>();
+            return app;
+        }
+    }
+}
---
+++ aspnetcore/labs/03/CoreMovies/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CoreMovies.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace CoreMovies.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; protected set; }
+
+        public IndexModel(IOptions<Greetings> greetings)
+        {
+            if(DateTime.UtcNow.Hour > 12) {
+                Message = greetings.Value.Evening;
+            }
+            else {
+                Message = greetings.Value.Morning;
+            }
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
