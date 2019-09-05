c0dcdef0910ac97a4f8c5803923c73002af14291
Scott Allen <scott@odetocode.com>
2019-09-05 10:16:14 -0400

    update aspnetcore lab 09 for EF 3

--- aspnetcore/labs/09/CoreMovies/CoreMovies.Web/Middlewares/ApplicationBuilderExtensions.cs
+++ aspnetcore/labs/09/CoreMovies/CoreMovies.Web/Middlewares/ApplicationBuilderExtensions.cs
@@ -4,7 +4,7 @@ namespace Microsoft.AspNetCore.Builder
 {
     public static class ApplicationBuilderExtensions
     {
-        public static IApplicationBuilder SetDefaultCsp(this IApplicationBuilder app)
+        public static IApplicationBuilder UseDefaultCsp(this IApplicationBuilder app)
         {
             app.UseMiddleware<DefaultCsp>();
             return app;
