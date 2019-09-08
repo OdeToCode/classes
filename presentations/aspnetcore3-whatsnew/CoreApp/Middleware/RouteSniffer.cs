b942c87ba0cc72c115569e987e80a91e00079967
Scott Allen <scott@odetocode.com>
2019-09-08 18:50:54 -0400

    sample app added to aspnetcore3-whatsnew

---
+++ presentations/aspnetcore3-whatsnew/CoreApp/Middleware/RouteSniffer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CoreApp.Middleware
+{
+    public class RouteSniffer
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<RouteSniffer> logger;
+
+        public RouteSniffer(RequestDelegate next, ILogger<RouteSniffer> logger)
+        {
+            this.next = next;
+            this.logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var feature = context.Features.Get<IEndpointFeature>();
+            
+            logger.LogInformation($"{feature.Endpoint.DisplayName}");
+            foreach(var data in feature.Endpoint.Metadata) 
+            {
+                var s = JsonSerializer.Serialize(data);
+                logger.LogInformation($"{ data.GetType().ToString()}");
+            }
+
+            await next(context);
+        }
+    }
+}
