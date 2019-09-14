4e6e4ab281f40e85111e5d5651fcc65e6bfa9630
Scott Allen <scott@odetocode.com>
2019-09-14 14:24:03 -0400

    working on aspnetcore3 deck

--- presentations/aspnetcore3-whatsnew/FeatureTests/runtime/JsonTests.cs
+++ presentations/aspnetcore3-whatsnew/FeatureTests/runtime/JsonTests.cs
@@ -41,6 +41,37 @@ namespace FeatureTests.runtime
         {
             var jsonText = "{ \"firstName\":\"Scott\", \"lastName\":\"Allen\" }";            
             using var document = JsonDocument.Parse(jsonText);
+
+            var result = document.RootElement.GetProperty("firstName").GetString();
+            Assert.Equal("Scott", result);
+        }
+
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public int Age { get; set; }
+        }
+
+        [Fact]
+        public void Can_Serialize()
+        {
+            var person = new Person { FirstName = "Lisa", Age = 42 };
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            
+            var json = JsonSerializer.Serialize(person, options);
+
+            Assert.StartsWith("{\"firstName\":\"Lisa\"", json);
+        }
+
+        [Fact]
+        public void Can_Deserialize()
+        {
+            var json = "{\"firstName\":\"Alex\", \"age\":20}";
+            var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+            var person = JsonSerializer.Deserialize<Person>(json, options);
+
+            Assert.Equal("Alex", person.FirstName);
+            Assert.Equal(20, person.Age);
         }
     }
 }
