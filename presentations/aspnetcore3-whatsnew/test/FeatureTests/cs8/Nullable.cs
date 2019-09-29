6aa76e34f0bf0dff664705b07baea75e8bee61ce
Scott Allen <scott@odetocode.com>
2019-09-29 19:50:10 -0400

    change nullable example

--- presentations/aspnetcore3-whatsnew/test/FeatureTests/cs8/Nullable.cs
+++ presentations/aspnetcore3-whatsnew/test/FeatureTests/cs8/Nullable.cs
@@ -2,15 +2,27 @@
 using Xunit;
 
 namespace FeatureTests
-{    
+{
+
+    public class Person
+    {
+        public string FirstName { get; set; } = null!;
+    }
+
     public class Nullable
     {
         [Fact]
         public void NullableContexts()
         {
 
+            var p = new Person();
+            if (p.FirstName != null)
+            {
+                var l = p.FirstName.Length;
+            }
+
 #nullable enable
-            string? x = null;
+            string x = null;
             Console.WriteLine(x);
 
 #nullable disable
