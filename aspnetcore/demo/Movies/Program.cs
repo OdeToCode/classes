f702ab4eace0413ff241b4f820488a6b82d94663
Scott Allen <scott@odetocode.com>
2019-09-30 04:38:17 -0400

    workshop part 1

---
+++ aspnetcore/demo/Movies/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder()
+                           .ConfigureWebHostDefaults(b =>
+                           {
+                               b.UseStartup<Startup>();
+                           })
+                           .ConfigureLogging(log =>
+                           {
+                               log.AddConsole();
+                               log.SetMinimumLevel(LogLevel.Information);
+                           })
+                           .ConfigureAppConfiguration(c =>
+                           {
+                               c.AddJsonFile("appsetting.json", optional: true);
+                               c.AddEnvironmentVariables();
+                           })
+                           .Build();
+            host.Run();
+        }
+    }
+}
---
+++ aspnetcore/demo/Movies/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Movies
+{
+    public class Startup 
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void Configure(
+            IApplicationBuilder app, 
+            ILogger<Startup> logger, 
+            IWebHostEnvironment environment)
+        {
+            if(environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
+            }
+            
+            app.UseStatusCodePages();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(e =>
+            {
+                e.MapGet("/error", ctx =>
+                {
+                    throw new Exception("Ouch!");
+                });
+
+                e.MapGet("/speak", async ctx =>
+                {
+                    logger.LogInformation("I wil speak...");
+                    await ctx.Response.WriteAsync(configuration["Greeting"] ?? "No message");
+                });
+            });
+
+            
+        }
+
+       
+    }
+}
